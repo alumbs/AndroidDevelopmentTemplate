5f8fd044919ca6e884cb28643f65a141590854bd
Chibuzor Alumba <coolchibs@gmail.com>
2015-08-15 13:48:22 -0500

    Removing test code that broke the build. Restoring the build to a working version

--- AndroidDevelopmentTemplate/app/src/main/java/development/scam/androiddevelopmenttemplate/MainActivity.java
+++ AndroidDevelopmentTemplate/app/src/main/java/development/scam/androiddevelopmenttemplate/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        feajo;fjeaio;jfeaoj;
+        
         setContentView(R.layout.activity_main);
     }
 
